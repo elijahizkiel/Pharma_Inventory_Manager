2501932584c4d552a0a15c644761456597e12a44
elijahizkiel <145738485+elijahizkiel@users.noreply.github.com>
2024-05-24 16:34:15 +0300

    private method getMetadata is added to PrescriptionNotifier

--- src/PrescriptionNotifier.java
+++ src/PrescriptionNotifier.java
@@ -22,13 +22,24 @@ public class PrescriptionNotifier implements Callable {
         }
     }
 
-    private void getMetaDataOfTable(){
-        DatabaseMetaData tableMetaData;
+    private ResultSet getMetaDataOfTable(){
+        ResultSet tableMetaData = null;
         try{
             DatabaseMetaData dbMetaData = connection.getMetaData();
+            tableMetaData = dbMetaData.getTables(null,null,"PrescriptionsRecords",null);
 
         }catch (SQLException e){
             System.out.println("can't getMetadata of prescription table " + e.getMessage());
+        }
+        return tableMetaData;
+    }
+
+    private void sortDataInTable(){
+        try{
+            Statement statement = connection.createStatement();
+            statement.execute("SELECT dateAndTime FROM PrescriptionsRecords ORDER BY dateAndTime ");
+        }catch (SQLException e){
+            System.out.println("can't sort data in table " + e.getMessage());
         }
     }
 
