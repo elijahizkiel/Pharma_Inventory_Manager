12b12e2f135874d9d903ac61507043a3cf6214a0
elijahizkiel <145738485+elijahizkiel@users.noreply.github.com>
2024-05-24 22:23:11 +0300

    getMetaData method replaced with getLastRowPointer method

--- src/PrescriptionNotifier.java
+++ src/PrescriptionNotifier.java
@@ -22,22 +22,26 @@ public class PrescriptionNotifier implements Callable {
         }
     }
 
-    private ResultSet getMetaDataOfTable(){
-        ResultSet tableMetaData = null;
+    private long getLastRowPointer(){
+        long dateAndTime = 0;
+        this.connect();
+        this.sortDataInTable();
         try{
-            DatabaseMetaData dbMetaData = connection.getMetaData();
-            tableMetaData = dbMetaData.getTables(null,null,"PrescriptionsRecords",null);
-
-        }catch (SQLException e){
-            System.out.println("can't getMetadata of prescription table " + e.getMessage());
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM PrescriptionsRecords");
+            if(result.isFirst()){
+                dateAndTime = result.getLong("dateAndTime");
+            }
+        }catch(SQLException e){
+            System.out.println("can't get dateAndTime for item in PrescriptionsRecords " + e.getMessage());
         }
-        return tableMetaData;
+        return dateAndTime;
     }
 
     private void sortDataInTable(){
         try{
             Statement statement = connection.createStatement();
-            statement.execute("SELECT dateAndTime FROM PrescriptionsRecords ORDER BY dateAndTime ");
+            statement.execute("SELECT dateAndTime FROM PrescriptionsRecords ORDER BY dateAndTime DSC");
         }catch (SQLException e){
             System.out.println("can't sort data in table " + e.getMessage());
         }
